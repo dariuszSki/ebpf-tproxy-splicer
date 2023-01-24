764b99c9d8a66ec1e33d87a17b15f65a1ac694bb
dariuszSki <dariusz.sliwinski@netfoundry.io>
2023-01-24 13:09:11 -0500

    fixed comment typos and syntax

--- files/scripts/tproxy_splicer_startup.sh
+++ files/scripts/tproxy_splicer_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # get local ip for lanIf
-# First check if theedge router config file exist
+# First check if the edge router config file exists
 router_config_file=/opt/netfoundry/ziti/ziti-router/config.yml
 if [ -f "$router_config_file" ]; then
     # Look up the value of lanIf
@@ -9,7 +9,7 @@ if [ -f "$router_config_file" ]; then
     if [ "$MYIF" ]; then
         # Then get IP
         MYIP=$(/sbin/ip add show "${MYIF}"|awk '$1=="inet" {print $2;}'|awk -v FS='/' '{print $1}')
-        # check if the ebpf is attached already
+        # check if the ebpf is attached 
         ATTACHED=`eval tc filter show dev $MYIF ingress`
         if [ $(echo  $ATTACHED | grep -w -c tproxy_splicer.o) == 1 ]; then
             echo "Found ebpf program tproxy_splicer and will re-add it to $MYIF now"
@@ -25,13 +25,13 @@ if [ -f "$router_config_file" ]; then
         fi
         # Save full path of map update into a variable
         map_update=/opt/netfoundry/ebpf/objects/map_update
-        # Ziti Fabric Port/Transport
+        # Get Ziti Fabric Port/Transport
         ZITI_FPORT=$(cat /opt/netfoundry/ziti/ziti-router/config.yml |yq .ctrl.endpoint |awk -v FS=':' '{print $3}')
         ZITI_FTRANSPORT=$(cat /opt/netfoundry/ziti/ziti-router/config.yml |yq .ctrl.endpoint |awk -v FS=':' '{print $1}')
         if [ "$ZITI_FTRANSPORT" == "tls" ]; then
             $map_update -I -c $MYIP -m 32 -l $ZITI_FPORT -h $ZITI_FPORT -t 0 -p tcp
         fi
-        # Ziti Client Port/Transport
+        # Get Ziti Client Port/Transport
         ZITI_CPORT=$(cat /opt/netfoundry/ziti/ziti-router/config.yml |yq '.listeners[] | select(.binding == "edge").address' |awk -v FS=':' '{print $3}')
         ZITI_CTRANSPORT=$(cat /opt/netfoundry/ziti/ziti-router/config.yml |yq '.listeners[] | select(.binding == "edge").address' |awk -v FS=':' '{print $1}')
         if [ "$ZITI_CTRANSPORT" == "tls" ]; then
@@ -39,7 +39,7 @@ if [ -f "$router_config_file" ]; then
         fi
         # DNS
         $map_update -I -c $MYIP -m 32 -l 53 -h 53 -t 0 -p udp
-        # Health-Checks
+        # Get Health-Check Port
         ZITI_HCPORT=$(cat /opt/netfoundry/ziti/ziti-router/config.yml |yq '.web[] | select(.name == "health-check").bindPoints[].address' | awk -v FS=':' '{print $2}')
         if [ "$ZITI_HCPORT" ]; then
             $map_update -I -c $MYIP -m 32 -l $ZITI_HCPORT -h $ZITI_HCPORT -t 0 -p tcp
